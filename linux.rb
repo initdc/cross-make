e7829dc1ca7056b0057c66ec67bafb2f8c8fc871
initdc <initd@outlook.com>
2023-01-02 16:19:40 +0000

    feat: add linux cross make logic

--- linux.rb
+++ linux.rb
@@ -1,1 +1,59 @@
 # frozen_string_literal: true
+
+require "libexec"
+
+require_relative "lib/error"
+require_relative "lib/deps"
+require_relative "lib/dir"
+
+CROSS_MAKE = true
+CONFIG = "x86_64_defconfig"
+CONFIGS = {
+  "x86_64_defconfig" => ["", ""],
+  # "starfive_visionfive2_defconfig" => ["riscv", "riscv64-linux-gnu-"],
+}.freeze
+FILES = "*.tar"
+
+Libexec.run("mkdir -p #{arg_dirs}")
+
+jammy_apt
+linux_deps
+install_cc if CROSS_MAKE
+
+Dir.chdir(BUILD_DIR) do
+  clone_cmd = "git clone https://github.com/andy-shev/linux.git --depth 1"
+
+  Libexec.run(clone_cmd) unless Libexec.code("test -d linux").zero?
+
+  Dir.chdir("linux") do
+    CONFIGS.each do |config, arr|
+      arch = arr[0]
+      cross = arr[1]
+
+      config_cmd = if arch.empty?
+                  "make #{config}"
+                else
+                  "ARCH=#{arch} make #{config}"
+                end
+      Libexec.code(config_cmd, Econfig)
+
+      make_cmd = if arch.empty?
+                   "make tar-pkg -j$(nproc)"
+                 else
+                   "ARCH=#{arch} CROSS_COMPILE=#{cross} make tar-pkg -j$(nproc)"
+                 end
+      result = Libexec.code(make_cmd).zero?
+      next unless result
+
+      pre = config.delete_suffix("_defconfig")
+      git_rev = Libexec.output("git rev-parse --short HEAD")
+
+      target = "../../#{TARGET_DIR}/linux/#{git_rev}/#{pre}"
+      Libexec.run("mkdir -p #{target}")
+      FILES.split(" ").each do |file|
+        Libexec.run("cp #{file} ../../#{UPLOAD_DIR}")
+        Libexec.run("mv #{file} #{target}")
+      end
+    end
+  end
+end
